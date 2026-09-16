4c9b1c9862758e0a86e39f1d04dc376c0f211c46
amol-chavhan <cs13engineer@gmail.com>
2026-09-16 23:05:04 +0530

    enhanced testing...

--- test/app.test.js
+++ test/app.test.js
@@ -1,30 +1,28 @@
 import request from 'supertest'
 import { expect } from 'chai'
 import app from '../src/app.js'
-import users from '../data/index.js';
-
 
 describe('GET /', () => {
-    const userList = [
-        {
-            username: "johnd",
-            name: "John Doe",
-            designation: "Developer",
-            hobies: ["Learning new things"]
-        }
-    ]
-    it('it should return 200', async () => {
+    it('should return the home page with 200', async () => {
         const response = await request(app).get('/')
 
         expect(response.status).to.equal(200)
-    });
+        expect(response.type).to.match(/html|text\/html/)
+    })
 
-    //get users test
-    it("it should return the user-list", async () => {
+    it('should return the user list with sanitized data', async () => {
         const response = await request(app).get('/users')
-        expect(response.statusCode).to.equal(200)
+
+        expect(response.status).to.equal(200)
         expect(response.body.status).to.equal(true)
-        expect(response.body.msg).to.equal("data fetched successfully")
-        expect(response.body.data).to.be.an('array')
-    });
+        expect(response.body.msg).to.equal('data fetched successfully')
+        expect(response.body.data).to.be.an('array').that.is.not.empty
+        expect(response.body.data[0]).to.include({
+            username: 'johnd',
+            name: 'John Doe',
+            designation: 'Developer'
+        })
+        expect(response.body.data[0]).to.have.property('hobies').that.deep.equals(['Learning new things'])
+        expect(response.body.data[0]).to.not.have.property('password')
+    })
 })
